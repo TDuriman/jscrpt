f9af053fcca20f05f8ebca471e1396d5a681df90
Timothy Daniell D. Duriman <durimantimotty13@gmail.com>
2023-07-24 20:41:32 +0800

    add+1

---
+++ scrpt.js
@@ -0,0 +1,35 @@
+// Lesson 3 Exercise - Supersimpledev(YouTube)
+
+//STRING
+
+//.3a-.3b-.3c
+
+"My name is: Timothy";
+
+//.3d
+
+"Total cost: $" +
+  (5 + 3)//.3e //output = $8
+
+  `Total cost: $${5 + 3}`; //output = $8
+
+//.3f
+
+alert(`Total cost: $${5 + 3}`);
+
+//.3g  Concatenation
+
+"Total cost: $" + (599 + 295) / 100;
+"Total cost: $8.94"//.3h Interpolation
+
+`Total cost: $${(599 + 295) / 100}`;
+("Total cost: $8.94");
+
+//.3i
+
+alert(`Total cost: $${(599 + 295) / 100}`);
+
+//.3j
+
+alert(`Total cost: $${(599 + 295) / 100}
+Thank you, come again!`);
